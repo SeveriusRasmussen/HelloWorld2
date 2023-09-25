cc7312129adce0501d1bfae9f320697d1869730d
Dosei-desu <krispauly@hotmail.com>
2023-09-25 13:01:56 +0200

    test change commit/push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej med jer kammerater!");
+        String change = "I have made a change";
+        System.out.println(change);
     }
 }
