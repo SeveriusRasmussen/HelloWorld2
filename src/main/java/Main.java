41d715755fc5c5291d84394db1fef3640ff0b2ba
SeveriusRasmussen <sora0001@stud.kea.dk>
2023-09-25 13:03:36 +0200

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej med jer kammerater!");
         String change = "I have made a change";
+        String haha = "Hahaha";
+        System.out.println(haha);
         System.out.println(change);
     }
 }
